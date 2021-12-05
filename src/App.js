ac9501d01a7bf9795bfe901f5bd003afb1a1087d
chepurniak <a.chepurniak@gmail.com>
2021-12-05 17:34:36 +0100

    install axios, json-server + small example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import './reset.scss';
 
 function App() {
+
+  const [colors, setColors] = useState(null);
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/colors').then(({ data }) => {
+      setColors(data);
+    });
+  }, []);
+
+
+
   return (
-    <h1>Hello World</h1>  
+    <>
+      {colors && colors.map(
+        ({id, hex, name}) => (
+          <p key={id}>
+            Color name:  <span style={{color: hex}}>{name}</span>, 
+            color hex: <span style={{color: hex}}>{hex}</span>
+          </p>
+        ))}
+    </>
   );
 }
 
